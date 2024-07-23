f4b04f630c032b5853e9887d5ccbb6c41330c502
jimmy <wjdtjq6@gmail.com>
2024-07-23 15:38:11 +0900

    Initial Commit

---
+++ LSLP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSLP
+//
+//  Created by t2023-m0032 on 7/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
